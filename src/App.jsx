c6ff3da47c55b8daabdf896976ac3eb431e44f80
Cristian Miron <cristian.mironv02@gmail.com>
2026-02-09 10:11:57 +0100

    Añadimos clase de login y retoques en las demás

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/login" element={<Login />} />
-      </Route>
+      </Route> 
     </Routes>
   );
 }
